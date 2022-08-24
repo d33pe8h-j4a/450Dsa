42e3a3900011f8affea5b81df2136ea4e30b7595
Deepesh Jha <jhadeepesh3@gmail.com>
2022-08-24 23:50:21 +0530

    Implemented the split of a circular linked list.

---
+++ Linked_list/SplitIntoTwo.cpp
@@ -0,0 +1,168 @@
+//{ Driver Code Starts
+/* Program to split a circular linked list into two halves */
+
+
+#include<stdio.h> 
+#include<stdlib.h> 
+ 
+/* structure for a Node */
+struct Node
+{
+  int data;
+  struct Node *next;
+  
+  Node(int x){
+      data = x;
+      next = NULL;
+  }
+}; 
+
+void splitList(struct Node *head, struct Node **head1_ref,   struct Node **head2_ref);
+ struct Node* newNode(int key)
+{
+    struct Node *temp = new Node(key);
+    
+    return temp;
+}
+/* Function to split a list (starting with head) into two lists.
+   head1_ref and head2_ref are references to head Nodes of 
+    the two resultant linked lists */
+
+void printList(struct Node *head)
+{
+  struct Node *temp = head; 
+  if(head != NULL)
+  {
+    do {
+      printf("%d ", temp->data);
+      temp = temp->next;
+    } while(temp != head);
+    printf("\n");
+  }
+}
+
+ 
+/* Driver program to test above functions */
+int main()
+{
+  
+   int t,n,i,x;
+    scanf("%d",&t);
+   while(t--)
+   {
+	   struct Node *temp,*head = NULL;
+       struct Node *head1 = NULL;
+       struct Node *head2 = NULL;
+	   scanf("%d",&n);
+	   scanf("%d",&x);
+	   head=new Node(x);
+	   temp=head;
+	   for(i=0;i<n-1;i++){
+	   scanf("%d",&x);
+	   
+	   temp->next=new Node(x);
+	   
+	   temp=temp->next;
+	       
+	   }
+       
+       temp->next=head;
+
+       splitList(head, &head1, &head2);
+
+    //  printf("\nFirst Circular Linked List");
+      printList(head1);  
+ 
+    //  printf("\nSecond Circular Linked List");
+      printList(head2);  
+   
+   }
+  return 0;
+}
+
+// } Driver Code Ends
+
+
+/* The structure of linked list is the following
+struct Node
+{
+  int data;
+  struct Node *next;
+  
+  Node(int x){
+      data = x;
+      next = NULL;
+  }
+};
+*/
+
+//  function which splits the circular linked list.  head is pointer
+// to head Node of given lined list.  head1_ref1 and *head_ref2
+// are pointers to head pointers of resultant two halves.
+
+int findlen(Node* &head){
+    if(head == NULL){
+        return 0;
+    }
+    Node* temp = head->next;
+    int count = 1;
+
+    while(temp!=head){
+        temp=temp->next;
+        count++;
+    }
+
+    return count;
+}
+
+Node* getMid(Node* head, int n) {
+    if (head) {
+        Node* temp = head;
+        while (n--)
+        {
+            temp = temp->next;
+        }
+        return temp;
+    }
+    return head;
+}
+
+Node* getMeprev(Node* &node, Node* &head) {
+    if (not(node and node != head)) return NULL;
+    Node* temp = head;
+    while (temp->next != node)
+    {
+        temp = temp->next;
+    }
+    return temp;
+}
+
+void splitList(Node *head, Node **head1_ref, Node **head2_ref)
+{
+    // your code goes here
+    int len = findlen(head);
+    *head1_ref = head;
+    if (len&1) {
+        Node* mid = getMid(head, len/2);
+        *head2_ref = mid->next;
+        mid->next = *head1_ref;
+        Node* temp = *head2_ref;
+
+        while (temp->next != head)
+        {
+            temp = temp->next;
+        }
+        temp->next = *head2_ref;
+    }
+    else {
+        Node* mid = getMid(head, len/2 - 1);
+        *head2_ref = mid->next;
+        mid->next = *head1_ref;
+        Node* temp = *head2_ref;
+        while (temp->next != head)
+        {
+            temp = temp->next;
+        }
+        temp->next = *head2_ref;
+    }
+}
